032fe16c8887e02e752ca9eb2f929a4ad10b9d28
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2025-04-26 17:06:01 -0600

    Begin setting up planter

---
+++ arborist/src/opportunity-api.ts
@@ -0,0 +1,10 @@
+export async function loadOpportunities(ids:number[]){
+    return [
+        {
+            id: 1,
+            completed: false,
+            abandoned: false,
+            text: String
+        }
+    ]
+}
