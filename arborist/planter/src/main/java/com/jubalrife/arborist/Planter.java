78a6e5b1bb8cd344781059b31bba8dd3e1bef563
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2025-04-27 16:18:36 -0600

    Some additional work on planter (may abandon it)

---
+++ arborist/planter/src/main/java/com/jubalrife/arborist/Planter.java
@@ -0,0 +1,14 @@
+package planter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Planter extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getOutputStream().print("Hello World");
+    }
+}
