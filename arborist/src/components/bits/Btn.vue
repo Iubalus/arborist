e62ad37285004818c37138300c3337966c759025
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2025-04-28 23:39:45 -0600

    Make terrifying bevel buttons

--- arborist/src/components/bits/Btn.vue
+++ arborist/src/components/bits/Btn.vue
@@ -44,12 +44,21 @@ export default defineComponent({
 button {
     font-family: monospace;
     text-align: center;
-    padding: 5px 10px;
+    padding: 3px 10px;
     box-sizing: border-box;
-    border-radius: 5px;
-    background: linear-gradient(360deg, rgb(129, 214, 253), white);
+    background: rgb(209, 240, 255);
     width: 100%;
     border: solid 1px rgb(128, 128, 128);
+    box-shadow: 0 0 1px 1px rgb(44, 44, 44);
+    border-top: 2px solid white;
+    border-left: 2px solid white;
+    border-right: 2px solid gray;
+    border-bottom: 2px solid gray;
+    text-shadow:
+        0 0 1px black,
+        1px 0px 1px white,
+        1px 1px 1px white;
+
 
     &.disabled {
         background: rgb(190, 190, 190);
@@ -57,8 +66,11 @@ button {
 
     &:hover:not(.disabled) {
         cursor: pointer;
-        box-shadow:inset 0 0 3px rgb(90, 90, 90);
-        background: linear-gradient(360deg, rgb(103, 165, 194), white);
+        border-top: 2px solid darkgray;
+        border-left: 2px solid darkgray;
+        border-right: 2px solid transparent;
+        border-bottom: 2px solid transparent;
+        background: rgb(175, 201, 214);
     }
 }
 </style>
