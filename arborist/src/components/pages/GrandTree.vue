795bc47bb34b97bc713d8ea868f2e5f68e7e180d
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2025-07-26 16:25:58 -0600

    Start work on a diferent tree render

--- arborist/src/components/pages/GrandTree.vue
+++ arborist/src/components/pages/GrandTree.vue
@@ -1,14 +1,124 @@
 <template>
     <Page title="Coming Soon">
-        Grand Tree Coming Soon
+        {{ depth }}
+        <table>
+            <tbody>
+                <tr v-for="level in fullTree(root)">
+                    <td
+                        v-for="node in level"
+                        :colspan="countLeaves(node)"
+                    >
+                        {{ node.content }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
     </Page>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Page from '../bits/Page.vue';
 
+interface Node {
+    contentType: "OUTCOME" | "JOINER" | "FILLER";
+    content?: Outcome | String;
+    children?: Node[];
+}
+
+interface Outcome {
+    type: "BUSINESS" | "PRODUCT",
+    text: String;
+}
+
 export default defineComponent({
-    components: { Page }
+    components: { Page },
+    data() {
+        return {            
+            root: {
+                contentType: "OUTCOME",
+                content: {
+                    type: "BUSINESS",
+                    text: "Profit"
+                },
+                children: [
+                    {
+                        contentType: "OUTCOME",
+                        content: {
+                            type: "BUSINESS",
+                            text: "Revenue"
+                        }
+                    },
+                    {
+                        contentType: "JOINER",
+                        content: "-"
+                    },
+                    {
+                        contentType: "OUTCOME",
+                        content: {
+                            type: "BUSINESS",
+                            text: "Cost"
+                        },
+                        children: [
+                            {
+                                contentType: "OUTCOME",
+                                content: {
+                                    type: "BUSINESS",
+                                    text: "Infra costs"
+                                }
+                            },
+                            {
+                                contentType: "JOINER",
+                                content: "+"
+                            },
+                            {
+                                contentType: "OUTCOME",
+                                content: {
+                                    type: "BUSINESS",
+                                    text: "People Cost"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            } as Node
+        }
+    },
+    computed: {
+        depth() {
+            return this.computeDepth(this.root, 0);
+        }
+    },
+    methods: {
+        countLeaves(node: Node): number {
+            if (!node.children) {
+                return 1;//leaf
+            }
+            return (node.children || []).map(c => this.countLeaves(c)).reduce((l, r) => l + r, 0);
+        },
+        computeDepth(node: Node, current: number): number {
+            return Math.max(...[current].concat((node.children || []).map(v => this.computeDepth(v, current + 1))));
+        },
+        fullTree(node: Node): Node[][] {
+            let nodes = [] as Node[][];
+            this.walkTree(node, 0, nodes);
+            return nodes;
+        },
+        walkTree(node: Node, level: number, flattened: Node[][]) {
+            flattened[level] = (flattened[level] || []).concat([node]);
+            if (!node.children) {
+                for (let i = level + 1; i <= this.depth; i++) {
+                    flattened[i] = (flattened[i] || []).concat([{contentType:"FILLER"}])
+                }
+            } else {
+                (node.children || [])?.forEach(v => this.walkTree(v, level + 1, flattened))
+            }
+        }
+    }
 })
 </script>
-<style scoped></style>
+<style scoped>
+td {
+    border: solid 1px blue;
+}
+</style>
