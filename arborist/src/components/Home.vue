4e2728300cd9d9a0e3d4768a97e240722fdd1104
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2025-08-09 14:22:56 -0600

    Improve page imports setup by adding an index

--- arborist/src/components/Home.vue
+++ arborist/src/components/Home.vue
@@ -11,18 +11,20 @@
 import { defineComponent, markRaw } from 'vue';
 import FlexRow from './bits/FlexRow.vue';
 import Page from './bits/Page.vue';
-import AssumptionMapping from './pages/AssumptionMapping.vue';
-import AssumptionTests from './pages/AssumptionTests.vue';
-import OpportunitySizing from './pages/OpportunitySizing.vue';
-import OpportunitySolutionTree from './pages/OpportunitySolutionTree.vue';
-import Outcomes from './pages/Outcomes.vue';
-import Overview from './pages/Overview.vue';
-import Playground from './pages/Playground.vue';
-import Questions from './pages/Questions.vue';
-import Snapshots from './pages/Snapshots.vue';
-import SolutionStorymap from './pages/SolutionStorymap.vue';
-import GrandTree from './pages/GrandTree.vue';
-import SystemMenu from './SystemMenu.vue';
+import {
+    AssumptionMapping,
+    AssumptionTests,
+    GrandTree,
+    OpportunitySizing,
+    OpportunitySolutionTree,
+    Outcomes,
+    Overview,
+    Playground,
+    Questions,
+    Snapshots,
+    SolutionStorymap,
+    SystemMenu
+} from './pages';
 
 export default defineComponent({
     components: { SystemMenu, FlexRow, Page },
