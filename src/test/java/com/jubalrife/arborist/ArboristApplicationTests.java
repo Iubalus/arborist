2ea1491430eae7090e5e3743f2f39cd1c5fd5d8c
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2026-03-10 21:19:43 -0600

    Create boilerplate spring-boot Java application and demo endpoint.

---
+++ src/test/java/com/jubalrife/arborist/ArboristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jubalrife.arborist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArboristApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
